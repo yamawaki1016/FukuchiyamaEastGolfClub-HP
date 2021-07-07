d0ba45947da01bd02dc7bff950147aa33eb187ea
yamawaki <junyama1204@outlook.jp>
2021-07-08 01:21:23 +0900

    Header対応

--- golf-club-hp/src/App.vue
+++ golf-club-hp/src/App.vue
@@ -1,18 +1,36 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <Header class="header" />
+  <div class="main">
+    <router-view />
   </div>
-  <router-view/>
 </template>
 
+<script>
+import Header from "./components/Header.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+  },
+};
+</script>
+
+
 <style lang="scss">
+body {
+  margin: 0px;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  // display: flex;
+  // flex-direction: column;
+  width: 100vm;
+  height: 100vh;
 }
 
 #nav {
@@ -26,5 +44,19 @@
       color: #42b983;
     }
   }
+}
+/** ------------------------------------- */
+.header {
+  height: 10vh;
+  margin-top: 5px;
+}
+.main {
+  // flex: 1;
+  background-color: rgb(245, 235, 231);
+}
+/** ------------------------------------- */
+ol,
+ul {
+  list-style: none;
 }
 </style>
